227b4eee5107c58f2b13ba6ac58dd94bc4e43442
Debbie Labedz <hello@bibliofille.com>
2014-10-02 12:08:13 -0500

    initial project commit, hooray

---
+++ footer.php
@@ -0,0 +1,24 @@
+?php
+/**
+ * The footer template
+ *
+ * Contains the closing of <div id="main"> and all content after.
+ *
+ * @package Deb
+ */
+?>
+
+    </div><!-- #main -->
+
+</div><!-- #page -->
+
+<footer id="colophon" role="contentinfo">
+    <div id="copyright">
+        <!-- copyright goes here -->
+        <a href="http://bibliofille.com" rel="nofollow">theme by YOU!</a>
+    </div>
+</footer><!-- #colophon -->
+
+<?php wp_footer(); ?> 
+</body>
+</html>
