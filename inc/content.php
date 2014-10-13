206450c1adeb500bcfb2d7d42140c7f01b873b59
Debbie Labedz <hello@bibliofille.com>
2014-10-13 14:57:43 -0500

    single post template

--- inc/content.php
+++ inc/content.php
@@ -5,15 +5,30 @@
  * @package Deb
  */
 ?>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
-        <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        <h1 class="entry-title">
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
         </h1>
         <span class="entry-date"><?php echo get_the_date(); ?></span>
     </header><!-- .entry-header -->
 
     <div class="entry-content">
         <?php the_content(); ?>
+        <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'deb' ) . '</span>', 'after' => '</div>' ) ); ?>
     </div><!-- .entry-content -->
 
+    <footer class="entry-meta">
+
+        <?php the_tags( '<div class="post-tags">' . __( 'Tagged: ', 'deb' ) , ', ', '</div>' ); ?>
+
+        <div class="comments-link">
+            <?php comments_popup_link( 
+                 __( 'Leave a comment', 'deb' ), 
+                 __( '1 comment', 'deb' ), 
+                 __( '% comments', 'deb' ) ); 
+            ?>
+        </div>
+    </footer><!-- #entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
