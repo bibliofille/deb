0e1197a131d3afd243f569195985f1ad42d395ba
Debbie Labedz <hello@bibliofille.com>
2014-10-13 10:08:45 -0500

    modified gulpfile.js and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -8,37 +8,38 @@ var gulp = require('gulp'),
     livereload = require('gulp-livereload'),
     lr = require('tiny-lr'),
     server = lr();
-
-    gulp.task('styles', function(){
-  return gulp.src(['scss/*.scss',
-                   'scss/**/*.scss'], 
-            {base: 'scss/'} )
-      .pipe(plumber())
-        .pipe(sass({ style: 'expanded' }))
-        .pipe(gulp.dest(''))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(minifycss())
-        .pipe(gulp.dest(''));
+ 
+    
+gulp.task('styles', function(){
+	return gulp.src('scss/style.scss', {base: 'scss'})
+	    .pipe(plumber())
+	    .pipe(sass({ style: 'expanded' }))
+	    .pipe(gulp.dest(''))
+	    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+	    .pipe(minifycss())
+	    .pipe(gulp.dest(''))
+	    .pipe(livereload(server));
 });
-
-    var imgSrc = 'assets/images/originals/*';
+ 
+ 
+var imgSrc = 'assets/images/originals/*';
 var imgDest = 'assets/images';
-
+ 
 gulp.task('images', function() {
   return gulp.src(imgSrc, {base: 'assets/images/originals'})
         .pipe(newer(imgDest))
         .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
         .pipe(gulp.dest(imgDest));
 });
-
+ 
 gulp.task('default', ['styles', 'images']);
-
+ 
 gulp.task('watch', function() {
   // Listen on port 35729
   server.listen(35729, function (err) {
       if (err) {
-        return console.log(err);
-      }
+        return console.log(err)
+      };
   
       // Watch .scss files
       gulp.watch('scss/*.scss', ['styles']);
@@ -46,5 +47,5 @@ gulp.task('watch', function() {
       gulp.watch('assets/images/originals/**', ['images']);
   
     });
-
+ 
 });
